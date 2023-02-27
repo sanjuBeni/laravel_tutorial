e515eb3364f6d30d9c64c10355cccd3dc452bf46
sanjay kumar <sanjay.kumar@actiknow.com>
2023-02-27 21:06:34 +0530

    migrate

---
+++ database/migrations/2023_02_27_152631_create_project_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->string('project_name');
+            $table->string('client_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project');
+    }
+};
